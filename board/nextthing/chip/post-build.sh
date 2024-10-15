c0fa3e46da788196c6de2c1cf6bde91c19e27c32
Guilhem Baccialone <guilhem.baccialone@murena.io>
2024-10-15 23:37:18 +0200

    chip: Fix post-build.sh that used a git Buildroot

--- board/nextthing/chip/post-build.sh
+++ board/nextthing/chip/post-build.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-TARGET_DIR=$1
-
-BRANCH=$(git symbolic-ref -q HEAD)
-BRANCH=${BRANCH##refs/heads/chip/}
-BRANCH=${BRANCH##refs/heads/nextthing/*/}
-
-BUILDROOT_GITHASH="$(cat buildroot_githash)"
-BUILDROOT_GITHASH="${BUILDROOT_GITHASH:0:8}"
-
-BUILD="$(cat build)"
-
 cat <<EOF >${TARGET_DIR}/etc/issue
-Welcome to CHIP Buildroot-${BRANCH} build ${BUILD} rev ${BUILDROOT_GITHASH}
+Welcome to CHIP Buildroot-2024.08
 
 CHIP Buildroot contains various open source software.
-
-The source code can be downloaded from:
-http://opensource.nextthing.co/chip/buildroot/${BRANCH}/${BUILD}/build${BUILD}.tar.gz
 
 EOF
